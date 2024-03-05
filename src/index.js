2829d7b5b4332699eb7482d66250d99e64213f88
madhu <rayi.madhupriya@gmail.com>
2024-03-05 21:52:33 +0530

    First commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const express= require('express');
+const db= require('./models/index');
+const app= express();
+
+const {PORT} = require('./config/serverConfig');
+const apiRoutes= require('./routes/index');
+const setupAndStartServer=(req,res)=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',apiRoutes);
+     app.listen(PORT,()=>{
+        console.log("Server started on port ",3003);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+     });
+}
+
+setupAndStartServer();
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express= require('express');
+
+const v1ApiRoutes= require('./v1/index');
+
+const router= express.Router();
+
+
+module.exports= router;
